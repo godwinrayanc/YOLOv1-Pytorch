b535e4ca1d6137a37785708f75aac64f6e1d35e7
godwinrayanc <74194500+godwinrayanc@users.noreply.github.com>
2021-06-14 16:40:45 +0200

    Added webcam script with comments

--- pytorch-yolov1-webcam/webcam_plot.py
+++ pytorch-yolov1-webcam/webcam_plot.py
@@ -1,22 +1,23 @@
+
+
+
+#Import dependencies for the opencv, load model and plotting functions
+
 import torch
 from torchvision import models
- 
-from newutils import (plot_image_pred,non_max_suppression,plot_image,cellboxes_to_boxes,convert_cellboxes,intersection_over_union)
 
+from newutils import (preprocess,
+    plot_image_pred,
+    non_max_suppression,
+    cellboxes_to_boxes,
+    convert_cellboxes,
+    intersection_over_union)
 
-import torchvision 
-from torch.autograd import Variable
 from torchvision import transforms
-import PIL 
-
-
 from bbone import resnet50
-#from head import Yolov1
 import cv2
-import argparse
-#from nets.nn import resnet50
+import PIL
 from PIL import Image 
-from torch.utils.data import DataLoader
 from functools import partial
 import pickle
 import pandas as pd
@@ -25,22 +26,13 @@ from torchvision import transforms
 
 
 
-
+#clear gpu memory
 torch.cuda.empty_cache()
 torch.backends.cudnn.benchmark = True
-#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-cline = argparse.ArgumentParser(description='YOLO v1 webcam detection demo')
-
-#cline.add_argument('-obj_thold', type=float, default=0.65,
-                   #help='threshold for objectness value')
-#cline.add_argument('-nms_thold', type=float, default=0.4,
-                   #help='threshold for non max supression')
-#cline.add_argument('-model_res', type=int, default=448,
-                   #help='resolution of the model\'s input')
 
 
 if __name__ == '__main__':
-    args = cline.parse_args()
+    
     with torch.no_grad():
         
         BATCH_SIZE = 64 
@@ -48,82 +40,43 @@ if __name__ == '__main__':
         PIN_MEMORY = True
 
 
-
-        torch.cuda.empty_cache()
-
         print('\nLOADING MODEL')
 
+        #define device to gpu and load model
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
         model = resnet50(split_size=7, num_boxes=2, num_classes=20, pretrained = False).to(device)
         
+        #load pretrained weights from data directory
         model.load_state_dict(torch.load('data/yolov1.pth'),strict=False)
         model.eval()
 
 
         print('\nMODEL LOADING COMPLETE')
 
-        classes = pd.read_csv('data/voc.names',header=None)
-
-        
-        # torch.cuda.empty_cache()
-        # loadweights = "data/yolov1.pth"
-
-        # model = resnet50(split_size=7,num_boxes=2,num_classes=20,pretrained=False).to(device)
-    
-        CAPTURE_SIZE = (448,448)
-        
-        data_transforms = transforms.Compose(
-                    [
-                        transforms.Resize(CAPTURE_SIZE),
-                        #transforms.CenterCrop(),
-                        #transforms.RandomHorizontalFlip(),
-                        transforms.ToTensor(),
-                        #transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0,0.225])
-                    ]
-                ) 
-        def preprocess(image):
-            image = PIL.Image.fromarray(image) #Webcam frames are numpy array format
-                                       #Therefore transform back to PIL image
-            print(image)                             
-            image = data_transforms(image)
-            image = image.float()
-             #image = Variable(image, requires_autograd=True)
-            image = image.cuda()
-            image = image.unsqueeze(0) #Resnet-50 model seems to only
-                               #accepts 4-D Vector Tensor so we need to squeeze another
-            return image
-
-
-
-        
-        while(True):
-            #_, image = cap.read()
+        NUM_FRAMES=200
+        for i in range(NUM_FRAMES):
+        #while(True):
+            
             print('\nCAPTURE FRAME by FRAME')
             webstream = cv2.VideoCapture(0)
             _, webstream = webstream.read()
 
-            
+
+            #preporcess webcam to pass through CNN
             x = preprocess(webstream)
+            
             print('\nPREDICTION')
-            #webstream = webstream.float()
+            
             x = x.to(device)
-            print('shape',x.shape)
             out = model(x)  
-            #print(image.shape)
             
             bboxes = cellboxes_to_boxes(out)
 
-            #print("Bounding boxes 0 len ",len(bboxes[0]))
-            #print("Bounding boxes0: ",bboxes[0])
-
-            #print("Bounding boxes len:",len(bboxes))
-            #print("Bounding boxes: ",bboxes)
-
             bboxes = non_max_suppression(bboxes[0],iou_threshold=0.5,threshold=0.4,box_format="midpoint")
-            print("Bounding boxes: ",bboxes)
             
+            #Convert 4D tensor back to 3D tensor for prediction
             x = x.squeeze(0)
-            #res = plot_image(webstream,bboxes,classes)
             res = plot_image_pred([x],boxes_pred=[bboxes])
 
 
